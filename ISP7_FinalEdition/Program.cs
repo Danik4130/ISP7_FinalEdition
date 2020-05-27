38acc31ab29008ef8140252da6660341a320fb05
Danik4130 <40251695+Danik4130@users.noreply.github.com>
2020-05-27 15:04:47 +0300

    Добавьте файлы проекта.

---
+++ ISP7_FinalEdition/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ISP7_FinalEdition
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
